74120a5502741b55b9772e5a2dcbe3bccafe3749
gpillusion <dnjscjf098@nate.com>
2014-01-10 22:53:11 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+## Build Instructions for E120S
+
+### Follow the usual instructions to download sources for CM11, e.g. :
+```
+1) mkdir cm11
+2) cd cm11
+3) repo init -u git://github.com/CyanogenMod/android.git -b cm-11.0
+```
+
+### Include the file .repo/local_manifests/local_manifest.xml to allow these additional repositories to be synced:
+```
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+<project name="CyanogenMod/android_device_samsung_msm8660-common" path="device/samsung/msm8660-common" remote="github" revision="cm-11.0" />
+<project name="CyanogenMod/android_device_samsung_celox-common" path="device/samsung/celox-common" remote="github" revision="cm-11.0" />
+<project name="CyanogenMod/android_device_samsung_qcom-common" path="device/samsung/qcom-common" remote="github" revision="cm-11.0" />
+<project name="CyanogenMod/android_hardware_samsung" path="hardware/samsung" remote="github" revision="cm-11.0" />
+<project name="TheMuppets/proprietary_vendor_samsung.git" path="vendor/samsung" remote="github" revision="cm-11.0" />
+<project name="gpillusion/android_device_samsung_e120s" path="device/samsung/e120s" revision="cm-11.0-e120s" />
+</manifest>
+```
+
+
+### Download or update all repositories:
+```
+repo sync
+```
+
+### Get all the prebuilts:
+```
+vendor/cm/get-prebuilts
+```
+
+### Ready to build!
+```
+source build/envsetup.sh
+brunch cm_e120s-eng
+```
---
+++ device.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+## The gps config appropriate for this device
+PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
+
+## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/skyrocket/skyrocket-vendor.mk)
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/e120s/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_COPY_FILES += \
+    device/samsung/e120s/init.qcom.usb.rc:root/init.qcom.usb.rc \
+
+# BT firmware
+PRODUCT_COPY_FILES += \
+    device/samsung/e120s/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
+
+# Inherit from celox-common
+$(call inherit-product, device/samsung/celox-common/celox-common.mk)
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/skyrocket/skyrocket-vendor.mk)
